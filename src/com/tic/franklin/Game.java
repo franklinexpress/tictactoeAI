b088d83fd1e7e431f75b070ca5449841d16ed039
Franklin De Los Santos <delossantos.franklin@gmail.com>
2017-06-12 12:43:49 -0700

    Created Game class to separate concerns.

---
+++ src/com/tic/franklin/Game.java
@@ -0,0 +1,83 @@
+package com.tic.franklin;
+
+import java.util.Scanner;
+
+/**
+ * Created by franklin on 6/12/17.
+ */
+public class Game {
+
+    Scanner sc;
+    Board board;
+
+    public Game(Scanner scanner, Board board) {
+        sc = scanner;
+        this.board = board;
+    }
+
+    public void gameLoop() {
+        while (true) {
+            System.out.println("\nPlay against the Computer (y/n) ? ");
+
+            if (sc.nextLine().equalsIgnoreCase("y"))
+                newGame(true);
+            else
+                newGame(false);
+
+            // at the end of the game, giving the user the option to try again
+            System.out.print("\n Play again (y/n) ? ");
+
+            //any input other than "Y" will terminate the program.
+            if (!sc.nextLine().equalsIgnoreCase("y")) {
+                break;
+            }
+        }
+    }
+
+    public  void newGame(boolean isSinglePlayer) {
+
+        board.displayBoard();
+
+        int player = Board.PLAYER_X; // plays first
+        while (!board.isGameOver()) {
+
+            Point point = null;
+
+            //try - because of weird or non numerical inputs
+            try {
+
+                if (isSinglePlayer && player == Board.PLAYER_O)
+                    point = board.miniMax(player);
+                else
+                    point = getMove(player);
+                if (!board.placeMove(point, player, false)) {
+                    System.out.println("Invalid Move");
+                    continue;
+                }
+                player = (player == Board.PLAYER_X ? Board.PLAYER_O : Board.PLAYER_X);
+                board.displayBoard();
+
+            } catch (Exception e) {
+                System.out.println("Invalid Input");
+            }
+        }
+        System.out.println(board.getWinner());
+    }
+
+
+    public  Point getMove(int player) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x;
+        int y;
+        String symbol = (player == Board.PLAYER_X ? "X" : "O");
+        System.out.print("\n Player " + symbol + " | Enter X Y values, space separated [0 - 2] -> ");
+
+        x = sc.nextInt();
+        y = sc.nextInt();
+        return new Point(x, y);
+    }
+
+
+}
--- src/com/tic/franklin/Main.java
+++ src/com/tic/franklin/Main.java
@@ -10,72 +10,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         //game loop
-        while (true) {
-            System.out.println("\nPlay against the Computer (y/n) ? ");
 
-            if (sc.nextLine().equalsIgnoreCase("y"))
-                newGame(true);
-            else
-                newGame(false);
-
-            // at the end of the game, giving the user the option to try again
-            System.out.print("\n Play again (y/n) ? ");
-
-            //any input other than "Y" will terminate the program.
-            if (!sc.nextLine().equalsIgnoreCase("y")) {
-                break;
-            }
-        }
     }
 
-
-    public static void newGame(boolean isSinglePlayer) {
-
-        Board board = new Board();
-        board.displayBoard();
-
-        int player = Board.PLAYER_X; // plays first
-        while (!board.isGameOver()) {
-
-            Point point = null;
-
-            //try - because of weird or non numerical inputs
-            try {
-
-                if (isSinglePlayer && player == Board.PLAYER_O)
-                    point = board.miniMax(player);
-                else
-                    point = getMove(player);
-
-                if (!board.placeMove(point, player, false)) {
-
-                    System.out.println("Invalid Move");
-                    continue;
-                }
-                player = (player == Board.PLAYER_X ? Board.PLAYER_O : Board.PLAYER_X);
-                board.displayBoard();
-
-            } catch (Exception e) {
-                System.out.println("Invalid Input");
-            }
-        }
-        System.out.println(board.getWinner());
-    }
-
-
-    public static Point getMove(int player) {
-
-        Scanner sc = new Scanner(System.in);
-
-        int x;
-        int y;
-        String symbol = (player == Board.PLAYER_X ? "X" : "O");
-        System.out.print("\n Player " + symbol + " | Enter X Y values, space separated [0 - 2] -> ");
-
-        x = sc.nextInt();
-        y = sc.nextInt();
-        return new Point(x, y);
-    }
 
 
 
